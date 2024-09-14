652eb413b0070882488a1072d41b378f9cef8e5a
Gustavo Leon <trabajoglm2021@gmail.com>
2024-09-14 08:30:24 -0700

    out

---
+++ out/_next/static/chunks/app/about/page-685df5b147840fa9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301],{9590:function(e,n,t){Promise.resolve().then(t.t.bind(t,132,23)),Promise.resolve().then(t.t.bind(t,8173,23)),Promise.resolve().then(t.t.bind(t,231,23))},132:function(){}},function(e){e.O(0,[906,30,971,23,744],function(){return e(e.s=9590)}),_N_E=e.O()}]);
